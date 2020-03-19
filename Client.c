d5fadd3eacfbabd93967d6f45e313470896bafea
Panchis7u7 <smadrigal.rod@gmail.com>
2020-03-18 19:08:47 -0600

    Definiendo estructura del archivo C

---
+++ Client.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <string.h>
+
+//(domain)AF_INET -> IPV4, (Type)SOCC_STREAM -> TCP, SOCK_DGRAM ->UDP, 
+//(Protocolo) 0 -> default TCP
+int main(int argc, char* argv[]){
+	//Crear un socket 
+	//Enlazar el socket una IP /puerto
+	//Marcar el socket para escuchar (listening)
+	//
+	return 0;
+}
