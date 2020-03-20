d8b0d3f5e9e6e43a474227d9f55e9333b580c2ea
Panchis7u7 <smadrigal.rod@gmail.com>
2020-03-19 20:32:34 -0600

    Servidor de mensajeria simple con un cliente

--- Server.c
+++ Server.c
@@ -8,27 +8,77 @@
 #include <arpa/inet.h>
 #include <string.h>
 
-int main(){
+//argv -> port number
+int main(int argc, char* argv[]){
+	if(argc < 2){
+		fprintf(stderr, "Numero de puerto no proporcionado, Termino del programa!");
+		exit(1);
+	}
+	
+	int listening_sock, new_listening_sock, port_number, n, i;
+	struct sockaddr_in server_address, client_address;
+	socklen_t client_length;
+	char buffer[255];
+	 
 	//Crear un socket 
-	int listening = socket(AF_INET, SOCK_STREAM, 0);
-	if(listening < 0){
+	if((listening_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
 		perror("Error al crear Socket!");
 		exit(EXIT_FAILURE);
 	}
+	
+	bzero((char*) &server_address, sizeof(server_address)); //Limpia cualquier dato 
+	//tambien se puede usar la funcion memset().
+	port_number = atoi(argv[1]);
+	
 	//Enlazar el socket a una IP / puerto
-	struct sockaddr_in hint;
-	hint.sin_family = AF_INET;
-	hint.sin_port = htons(54000);	//little endian en el puerto 54000
-	//pton -> AF_INET formato que se dara,  
-	inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr); //converts number to an array of integers
-	if(bind(listening, (struct sockaddr*) &hint, sizeof(hint)) < 0){
-		perror("No se pudo enlazar a la IP / Puerto!");
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = INADDR_ANY; 
+	/*Permite que el servidor trabaje sin conocer la direccion IP
+	sin_addr is the IP address in the socket, 
+	s_addr is variable that holds the information about the address 
+	we agree to accept. So, in this case i put INADDR_ANY because 
+	i would like to accept connections from any internet address. 
+	This case is used about server example. 
+	In a client example i could NOT accept connections from ANY ADDRESS.*/
+	server_address.sin_port = htons(port_number); //little endian en el puerto gracias al manejo de memoria arriba de 255 en i386
+	
+	if((bind(listening_sock, (struct sockaddr*) &server_address, sizeof(server_address))) < 0){
+		perror("Enlazamiento fallido!");
 		exit(EXIT_FAILURE);
 	}
+	
+	client_length = sizeof(client_address);
+	
 	//Marcar el socket para escuchar (listening)
-	if(listen(listening, SOMAXCONN) < 0){
+	if((listen(listening_sock, SOMAXCONN)) < 0){
 		perror("No se puede escuchar!");
 		exit(EXIT_FAILURE);
+	} 
+	
+	if((new_listening_sock = accept(listening_sock, (struct sockaddr*) &client_address, &client_length)) < 0){
+		perror("Problema con el cliente!");
+		exit(EXIT_FAILURE);
 	}
+	
+	while(1){
+		bzero(buffer, 255);
+		if((n = read(new_listening_sock, buffer, 255)) < 0){
+			perror("Error al leer los datos!");
+			exit(EXIT_FAILURE);
+		}
+		printf("Client: %s\n", buffer);
+		bzero(buffer, 255);
+		fgets(buffer, 255, stdin);
+		if((n = write(new_listening_sock, buffer, strlen(buffer))) < 0) {
+			perror("Problema al escribir los datos!");
+			exit(EXIT_FAILURE);
+		}
+		
+		if((i = strncmp("Adios", buffer, 5)) == 0){
+			break;
+		} 
+	}
+	//pton -> AF_INET formato que se dara,  
+	//inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr); //converts number to an array of integers
 	return 0;
 }
